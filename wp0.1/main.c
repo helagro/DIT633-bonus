6d2972b328c5d60c33f4a75d22442cb15aec9ea9
Henrik L <helagro@outlook.com>
2024-01-16 21:22:15 +0100

    unspecified

--- wp0.1/main.c
+++ wp0.1/main.c
@@ -1,15 +1,16 @@
 // Includes the standard input/output library
 #include <stdio.h>
+// Includes the string library
 #include <string.h>
 
-#define MIN_NAME_LENGTH 3
-#define MAX_NAME_LENGTH 50
+#define MIN_NAME_LENGTH 3 // Defines min length
+#define MAX_NAME_LENGTH 50 // Defines max length
 
 // The entry point of the program
 int main(const int argc, const char *argv[]){
     if(argc < 2){ // If there are no command line arguments
-        printf("No command line argument provided, please provide a name.\n");
-        return 1; // Returns 1 to indicate an error
+        printf("No command line argument provided, please provide a name. \n");
+        return 0; // Stops execution
     }
 
     // Gets the length of the first argument
@@ -18,14 +19,12 @@ int main(const int argc, const char *argv[]){
     // If the length of the first argument is shorter than the minimum allowed
     if(nameLength < MIN_NAME_LENGTH){
         printf("Name too short, min %d characters\n", MIN_NAME_LENGTH);
-        return 1; // Returns 1 to indicate an error
 
     // If the length of the first argument is longer than the maximum allowed
     } else if (nameLength > MAX_NAME_LENGTH){
         printf("Name too long, max %d characters\n", MAX_NAME_LENGTH);
-        return 1; // Returns 1 to indicate an error
+        
+    } else {
+        printf("Hello %s, how is your day today?\n", argv[1]);
     }
-    
-    // Prints "hello world" to the console
-    printf("Hello %s, how is your day today?\n", argv[1]);
 }
