c84b31f15ce1a501f7f21a146aa83e7d1d465128
Henrik L <helagro@outlook.com>
2024-01-22 10:53:44 +0100

    Solve wp_1_0

--- wp_1_0/main.c
+++ wp_1_0/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(const int argc, const char **argv) {
-    int n = 0;
+    u_int32_t n = 0;
     char c = argv[1][0];
     int i = 0;
 
@@ -12,7 +12,25 @@ int main(const int argc, const char **argv) {
         c = argv[1][++i];
     } while (c != '\0');
 
-    printf("Binary: 0b%d", n << 1);
+    const u_int32_t flipper = 0b10000000000000000000000000000000;
+    printf("Binary: 0b");
 
-    printf("%d\n", n);
+    int zeros = 0;
+    int ones = 0;
+
+    const int s = sizeof(u_int32_t) * 8 + 1;
+    for (int i2 = 0; i < s; i++) {
+        const int bit = (n & flipper) != 0;
+
+        if (bit)
+            ones++;
+        else if (ones > 0)
+            zeros++;
+
+        if (ones > 0)
+            printf("%d", bit);
+        n = n << 1;
+    }
+
+    printf("\nNumber of 0s: %d\nNumber of 1s: %d\n", zeros, ones);
 }
